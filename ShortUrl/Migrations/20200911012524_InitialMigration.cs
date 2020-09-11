9d73a054b22aadd611d6740152c0eba7a88bc803
KeepD0wn <38407409+KeepD0wn@users.noreply.github.com>
2020-09-11 04:27:42 +0300

    создание бд

---
+++ ShortUrl/Migrations/20200911012524_InitialMigration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace ShortUrl.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Links",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    LongURL = table.Column<string>(type: "varchar(300)", nullable: false),
+                    ShortUrl = table.Column<string>(type: "varchar(100)", nullable: false),
+                    CreatedData = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Count = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Links", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Links_ShortUrl",
+                table: "Links",
+                column: "ShortUrl",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Links");
+        }
+    }
+}
